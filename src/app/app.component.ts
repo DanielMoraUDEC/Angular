a18d6cb6a604309a3948462512a35b182d51fe4e
DanielMoraUDEC <djosemora@ucundinamarca.edu.co>
2021-09-07 08:29:58 -0500

    Comunicación entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mi primera pagina';
   texto;
+
+  numero1:number;
+  numero2:number;
+  resultado:Number;
+
+  public operar(oper): void{
+    console.log(this.numero1);
+    console.log(this.numero2);
+
+    switch (oper){
+      case 's' : this.resultado = this.numero1+this.numero2;
+      break;
+      case 'r' : this.resultado = this.numero1-this.numero2;
+      break;
+      case 'm': this.resultado = this.numero1*this.numero2;
+      break;
+      case 'd' : this.resultado = this.numero1/this.numero2;
+      break;
+      default: this.resultado = 0;
+    }
+
+    
+  }
+
+
 }
+
+
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
